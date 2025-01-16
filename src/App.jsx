adf417e0283b6281ed529f0aa9bb6f817f34f0b6
Ghost055 <mubarakzak055@gmail.com>
2025-01-16 02:09:42 +0000

    fix issues in App.jsx and home .jsx before switching to about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';  // Import AboutUs component
+import AboutUs from './pages/AboutUs';
 import NewsAnnouncement from './pages/NewsAnnouncement';
 import EventCalendar from './pages/EventCalendar';
 import EventGallery from './pages/EventGallery';
@@ -9,21 +9,31 @@ import Membership from './pages/Membership';
 import ContactUs from './pages/ContactUs';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
+import Comp from './components/Comp';
+import Members from './components/Members';
+import Register from './components/Register';  // Import Register component
+import Projects from './components/Projects';  // Import Projects component
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px[7vw] lg:px-[9vw]'>
       <Navbar />
-      <Hero /> {/* Hero section */}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about-us' element={<AboutUs />} /> {/* About Us Route */}
+        <Route path='/about-us' element={<AboutUs />} />
         <Route path='/news-announcement' element={<NewsAnnouncement />} />
         <Route path='/event-calendar' element={<EventCalendar />} />
         <Route path='/event-gallery' element={<EventGallery />} />
         <Route path='/membership' element={<Membership />} />
         <Route path='/contact-us' element={<ContactUs />} />
       </Routes>
+      <Hero />
+      <About />
+      <Comp />
+      <Members />
+      <Register />  {/* Register component */}
+      <Projects />  {/* Add the Projects component here */}
     </div>
   );
 };
